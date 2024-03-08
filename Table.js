42d3e609625076802297499d8f16aa2973863efa
Usama-Abbasi0001 <157971981+Usama-Abbasi0001@users.noreply.github.com>
2024-03-08 14:41:45 +0500

    Table

---
+++ Table.js
@@ -0,0 +1,332 @@
+let main = document.querySelector("body");
+let mainTable = document.createElement("div");
+main.append(mainTable);
+mainTable.style.border="3px solid #ED4C67";
+mainTable.style.width="1200px";
+mainTable.style.borderRadius="20px";
+mainTable.style.margin="auto";
+mainTable.style.display="flex";
+mainTable.style.justifyContent="space-around";
+mainTable.style.height="1000px";
+mainTable.style.alignItems="center";
+mainTable.style.background="#9980FA";
+
+let mainRow = document.createElement("div");
+mainTable.append(mainRow);
+
+mainRow.style.width="320px";
+mainRow.style.borderRadius="20px";
+
+mainRow.style.height="900px";
+
+
+let row = document.createElement("div");
+mainRow.append(row);
+row.style.width="300px";
+row.style.height="40px";
+row.style.borderRadius="20px";
+row.style.margin="auto";
+row.style.textAlign="center"
+row.style.fontSize="30px"
+row.style.paddingTop="10px"
+row.style.color="white"
+row.style.marginTop="20px"
+row.style.background="#FDA7DF";
+row.innerHTML="Name"
+
+let usama = document.createElement("div");
+mainRow.append(usama);
+usama.style.width="300px";
+usama.style.height="40px";
+usama.style.margin="auto";
+usama.style.textAlign="center"
+usama.style.fontSize="30px"
+usama.style.paddingTop="10px"
+usama.style.marginTop="20px"
+usama.style.background="#FDA7DF";
+usama.innerHTML="Usama"
+
+let Areeb = document.createElement("div");
+mainRow.append(Areeb);
+Areeb.style.width="300px";
+Areeb.style.height="40px";
+Areeb.style.margin="auto";
+Areeb.style.textAlign="center"
+Areeb.style.fontSize="30px"
+Areeb.style.paddingTop="10px"
+Areeb.style.marginTop="20px"
+Areeb.style.background="#FDA7DF";
+Areeb.innerHTML="Areeb"
+
+let Rehman = document.createElement("div");
+mainRow.append(Rehman);
+Rehman.style.width="300px";
+Rehman.style.height="40px";
+Rehman.style.margin="auto";
+Rehman.style.textAlign="center"
+Rehman.style.fontSize="30px"
+Rehman.style.paddingTop="10px"
+Rehman.style.marginTop="20px"
+Rehman.style.background="#FDA7DF";
+Rehman.innerHTML="Rehman"
+
+let Huzaifa = document.createElement("div");
+mainRow.append(Huzaifa);
+Huzaifa.style.width="300px";
+Huzaifa.style.height="40px";
+Huzaifa.style.margin="auto";
+Huzaifa.style.textAlign="center"
+Huzaifa.style.fontSize="30px"
+Huzaifa.style.paddingTop="10px"
+Huzaifa.style.marginTop="20px"
+Huzaifa.style.background="#FDA7DF";
+Huzaifa.innerHTML="Huzaifa"
+
+let Hanzala = document.createElement("div");
+mainRow.append(Hanzala);
+Hanzala.style.width="300px";
+Hanzala.style.height="40px";
+Hanzala.style.margin="auto";
+Hanzala.style.textAlign="center"
+Hanzala.style.fontSize="30px"
+Hanzala.style.paddingTop="10px"
+Hanzala.style.marginTop="20px"
+Hanzala.style.background="#FDA7DF";
+Hanzala.innerHTML="Hanzala";
+
+let Usaid = document.createElement("div");
+mainRow.append(Usaid);
+Usaid.style.width="300px";
+Usaid.style.height="40px";
+Usaid.style.margin="auto";
+Usaid.style.textAlign="center"
+Usaid.style.fontSize="30px"
+Usaid.style.paddingTop="10px"
+Usaid.style.marginTop="20px"
+Usaid.style.background="#FDA7DF";
+Usaid.innerHTML="Usaid";
+
+let Adnan = document.createElement("div");
+mainRow.append(Adnan);
+Adnan.style.width="300px";
+Adnan.style.height="40px";
+Adnan.style.margin="auto";
+Adnan.style.textAlign="center"
+Adnan.style.fontSize="30px"
+Adnan.style.paddingTop="10px"
+Adnan.style.marginTop="20px"
+Adnan.style.background="#FDA7DF";
+Adnan.innerHTML="Adnan";
+
+
+
+let sec = document.createElement("div");
+mainTable.append(sec);
+sec.style.width="320px";
+sec.style.borderRadius="20px";
+sec.style.height="900px";
+
+
+let colo = document.createElement("div");
+sec.append(colo);
+colo.style.width="300px";
+colo.style.height="40px";
+colo.style.borderRadius="20px";
+colo.style.margin="auto";
+colo.style.textAlign="center"
+colo.style.fontSize="30px"
+colo.style.color="white"
+colo.style.paddingTop="10px"
+colo.style.marginTop="20px"
+colo.style.background="#FDA7DF";
+colo.innerHTML="Department"
+
+let comp = document.createElement("div");
+sec.append(comp);
+comp.style.width="300px";
+comp.style.height="40px";
+comp.style.margin="auto";
+comp.style.textAlign="center"
+comp.style.fontSize="30px"
+comp.style.paddingTop="10px"
+comp.style.marginTop="20px"
+comp.style.background="#FDA7DF";
+comp.innerHTML="Computer Science"
+
+let soft = document.createElement("div");
+sec.append(soft);
+soft.style.width="300px";
+soft.style.height="40px";
+soft.style.margin="auto";
+soft.style.textAlign="center"
+soft.style.fontSize="30px"
+soft.style.paddingTop="10px"
+soft.style.marginTop="20px"
+soft.style.background="#FDA7DF";
+soft.innerHTML="Softwear Engineer"
+
+let softOne = document.createElement("div");
+sec.append(softOne);
+softOne.style.width="300px";
+softOne.style.height="40px";
+softOne.style.margin="auto";
+softOne.style.textAlign="center"
+softOne.style.fontSize="30px"
+softOne.style.paddingTop="10px"
+softOne.style.marginTop="20px"
+softOne.style.background="#FDA7DF";
+softOne.innerHTML="Softwear Engineer";
+
+let softTwo = document.createElement("div");
+sec.append(softTwo);
+softTwo.style.width="300px";
+softTwo.style.height="40px";
+softTwo.style.margin="auto";
+softTwo.style.textAlign="center"
+softTwo.style.fontSize="30px"
+softTwo.style.paddingTop="10px"
+softTwo.style.marginTop="20px"
+softTwo.style.background="#FDA7DF";
+softTwo.innerHTML="Computer Science";
+
+let softthr = document.createElement("div");
+sec.append(softthr);
+softthr.style.width="300px";
+softthr.style.height="40px";
+softthr.style.margin="auto";
+softthr.style.textAlign="center"
+softthr.style.fontSize="30px"
+softthr.style.paddingTop="10px"
+softthr.style.marginTop="20px"
+softthr.style.background="#FDA7DF";
+softthr.innerHTML="Computer Science";
+
+let softfor = document.createElement("div");
+sec.append(softfor);
+softfor.style.width="300px";
+softfor.style.height="40px";
+softfor.style.margin="auto";
+softfor.style.textAlign="center"
+softfor.style.fontSize="30px"
+softfor.style.paddingTop="10px"
+softfor.style.marginTop="20px"
+softfor.style.background="#FDA7DF";
+softfor.innerHTML="Computer Science";
+
+let softfiv = document.createElement("div");
+sec.append(softfiv);
+softfiv.style.width="300px";
+softfiv.style.height="40px";
+softfiv.style.margin="auto";
+softfiv.style.textAlign="center"
+softfiv.style.fontSize="30px"
+softfiv.style.paddingTop="10px"
+softfiv.style.marginTop="20px"
+softfiv.style.background="#FDA7DF";
+softfiv.innerHTML="Softwear Engineer";
+
+
+let third = document.createElement("div");
+mainTable.append(third);
+third.style.width="320px";
+third.style.borderRadius="20px";
+third.style.height="900px";
+
+
+let bye = document.createElement("div");
+third.append(bye);
+bye.style.width="300px";
+bye.style.height="40px";
+bye.style.borderRadius="20px";
+bye.style.margin="auto";
+bye.style.textAlign="center"
+bye.style.fontSize="30px"
+bye.style.color="white"
+bye.style.paddingTop="10px"
+bye.style.marginTop="20px"
+bye.style.background="#FDA7DF";
+bye.innerHTML="R:No"
+
+let byeone = document.createElement("div");
+third.append(byeone);
+byeone.style.width="300px";
+byeone.style.height="40px";
+byeone.style.margin="auto";
+byeone.style.textAlign="center"
+byeone.style.fontSize="30px"
+byeone.style.paddingTop="10px"
+byeone.style.marginTop="20px"
+byeone.style.background="#FDA7DF";
+byeone.innerHTML="2k22-cs-44"
+
+let byeonel = document.createElement("div");
+third.append(byeonel);
+byeonel.style.width="300px";
+byeonel.style.height="40px";
+byeonel.style.margin="auto";
+byeonel.style.textAlign="center"
+byeonel.style.fontSize="30px"
+byeonel.style.paddingTop="10px"
+byeonel.style.marginTop="20px"
+byeonel.style.background="#FDA7DF";
+byeonel.innerHTML="2k22-cs-32";
+
+let byeoneu = document.createElement("div");
+third.append(byeoneu);
+byeoneu.style.width="300px";
+byeoneu.style.height="40px";
+byeoneu.style.margin="auto";
+byeoneu.style.textAlign="center"
+byeoneu.style.fontSize="30px"
+byeoneu.style.paddingTop="10px"
+byeoneu.style.marginTop="20px"
+byeoneu.style.background="#FDA7DF";
+byeoneu.innerHTML="2k22-cs-22";
+
+let byeonew = document.createElement("div");
+third.append(byeonew);
+byeonew.style.width="300px";
+byeonew.style.height="40px";
+byeonew.style.margin="auto";
+byeonew.style.textAlign="center"
+byeonew.style.fontSize="30px"
+byeonew.style.paddingTop="10px"
+byeonew.style.marginTop="20px"
+byeonew.style.background="#FDA7DF";
+byeonew.innerHTML="2k22-cs-42";
+
+let byeones = document.createElement("div");
+third.append(byeones);
+byeones.style.width="300px";
+byeones.style.height="40px";
+byeones.style.margin="auto";
+byeones.style.textAlign="center"
+byeones.style.fontSize="30px"
+byeones.style.paddingTop="10px"
+byeones.style.marginTop="20px"
+byeones.style.background="#FDA7DF";
+byeones.innerHTML="2k22-cs-36";
+
+let byeonez = document.createElement("div");
+third.append(byeonez);
+byeonez.style.width="300px";
+byeonez.style.height="40px";
+byeonez.style.margin="auto";
+byeonez.style.textAlign="center"
+byeonez.style.fontSize="30px"
+byeonez.style.paddingTop="10px"
+byeonez.style.marginTop="20px"
+byeonez.style.background="#FDA7DF";
+byeonez.innerHTML="2k22-cs-16";
+
+let byeonea = document.createElement("div");
+third.append(byeonea);
+byeonea.style.width="300px";
+byeonea.style.height="40px";
+byeonea.style.margin="auto";
+byeonea.style.textAlign="center"
+byeonea.style.fontSize="30px"
+byeonea.style.paddingTop="10px"
+byeonea.style.marginTop="20px"
+byeonea.style.background="#FDA7DF";
+byeonea.innerHTML="2k22-cs-16";
